9fc7d6e0b95ba93f3c3500cf23ef09c3e27e0e5e
Hadi Sutomo Wonosantoso <89561203+hadisutomo@users.noreply.github.com>
2024-06-09 10:36:46 +0700

    Add files via upload
    menambahkan file MyToken.sol

---
+++ MyToken.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MyToken is ERC20 {
+
+    constructor() ERC20("DegenCat", "CAT") {
+        _mint(msg.sender, 100 * 10 ** ERC20.decimals());
+    }
+}
+
