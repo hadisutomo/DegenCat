91958dc0a24d22cc5d347f0183f515a41e8c54e5
Hadi Sutomo Wonosantoso <89561203+hadisutomo@users.noreply.github.com>
2024-06-09 10:52:31 +0700

    Update MyToken.sol
    changed the initial supply of tokens to 1 billion

--- MyToken.sol
+++ MyToken.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract MyToken is ERC20 {
 
     constructor() ERC20("DegenCat", "CAT") {
-        _mint(msg.sender, 100 * 10 ** ERC20.decimals());
+        _mint(msg.sender, 1000000000 * 10 ** ERC20.decimals());
     }
 }
 
